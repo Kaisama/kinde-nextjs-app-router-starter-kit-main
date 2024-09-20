a96ccb66f876e3331dfddec0c08546b049eee561
Kaisama <bishtakshat64@gmail.com>
2024-09-20 15:59:18 +0530

    c

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Remove the "output: export" configuration
-    // output: "export",
     serverRuntimeConfig: {
       port: 9124,
     },
   };
   
-  export default nextConfig;
+  module.exports = nextConfig;
